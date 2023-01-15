ebe68ed3d583702ba4ab74346e14626d6f12efc0
Lust3r <s1skyblue1s@gmail.com>
2023-01-15 13:36:22 +0900

    Refactor: Stepper Value Change시 store의 재고 수량 변경

--- JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
+++ JuiceMaker/JuiceMaker/Controller/FruitViewController.swift
@@ -22,9 +22,63 @@ class FruitViewController: UIViewController, SendDataDelegate {
     
     @IBOutlet var fruitEmojiBundle: [UILabel]!
     @IBOutlet var fruitStoreCountBundle: [UILabel]!
-    @IBOutlet var stepperBundle: [UIStepper]!
+    @IBOutlet weak var strawberryStepper: UIStepper!
+    @IBOutlet weak var bananaStepper: UIStepper!
+    @IBOutlet weak var pineAppleStepper: UIStepper!
+    @IBOutlet weak var kiwiStepper: UIStepper!
+    @IBOutlet weak var mangoStepper: UIStepper!
     
-    @IBAction func stepperAction(_ sender: UIStepper) {
+    private func initSteppers() {
+        strawberryStepper.value = Double(fruitStore.sendBackToAvailableStock(fruit: .strawberry))
+        bananaStepper.value = Double(fruitStore.sendBackToAvailableStock(fruit: .banana))
+        pineAppleStepper.value = Double(fruitStore.sendBackToAvailableStock(fruit: .pineApple))
+        kiwiStepper.value = Double(fruitStore.sendBackToAvailableStock(fruit: .kiwi))
+        mangoStepper.value = Double(fruitStore.sendBackToAvailableStock(fruit: .mango))
+    }
+    
+    @IBAction func strawberryStepper(_ sender: UIStepper) {
+        fruitStoreCountBundle.forEach { label in
+            if label.accessibilityLabel == "strawberry" {
+                fruitStore.store.updateValue(Int(sender.value), forKey: .strawberry)
+                label.text = String(fruitStore.sendBackToAvailableStock(fruit: .strawberry))
+            }
+        }
+    }
+    
+    @IBAction func bananaStepper(_ sender: UIStepper) {
+        fruitStoreCountBundle.forEach { label in
+            if label.accessibilityLabel == "banana" {
+                fruitStore.store.updateValue(Int(sender.value), forKey: .banana)
+                label.text = String(fruitStore.sendBackToAvailableStock(fruit: .banana))
+            }
+        }
+    }
+    
+    @IBAction func pineAppleStepper(_ sender: UIStepper) {
+        fruitStoreCountBundle.forEach { label in
+            if label.accessibilityLabel == "pineApple" {
+                fruitStore.store.updateValue(Int(sender.value), forKey: .pineApple)
+                label.text = String(fruitStore.sendBackToAvailableStock(fruit: .pineApple))
+            }
+        }
+    }
+    
+    @IBAction func kiwiStepper(_ sender: UIStepper) {
+        fruitStoreCountBundle.forEach { label in
+            if label.accessibilityLabel == "kiwi" {
+                fruitStore.store.updateValue(Int(sender.value), forKey: .kiwi)
+                label.text = String(fruitStore.sendBackToAvailableStock(fruit: .kiwi))
+            }
+        }
+    }
+    
+    @IBAction func mangoStepper(_ sender: UIStepper) {
+        fruitStoreCountBundle.forEach { label in
+            if label.accessibilityLabel == "mango" {
+                fruitStore.store.updateValue(Int(sender.value), forKey: .mango)
+                label.text = String(fruitStore.sendBackToAvailableStock(fruit: .mango))
+            }
+        }
     }
     
     private let fruitStore = FruitStore.shared
@@ -33,4 +87,6 @@ class FruitViewController: UIViewController, SendDataDelegate {
         super.viewDidLoad()
         syncFruitStocks()
         fruitStore.delegate = self
+        initSteppers()
     }
+}
